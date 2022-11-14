f1195022801197b49a5893ffb9dc19f8b45abce6
oksunwoo <oksunwo@naver.com>
2022-11-14 17:38:15 +0900

    initial commit

---
+++ TCA_Practice/TCA_Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA_Practice
+//
+//  Created by Sunwoo on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
