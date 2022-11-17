4d9f7e478771eb1b730fc731f13b2514cec49945
oksunwoo <oksunwo@naver.com>
2022-11-17 18:49:32 +0900

    feat: cancellation View 구현

--- TCA_Practice/TCA_Practice/EffectsCancellationView.swift
+++ TCA_Practice/TCA_Practice/EffectsCancellationView.swift
@@ -29,18 +29,85 @@ struct EffectsCancellation: ReducerProtocol {
         case .cancelButtonTapped:
             state.isFactRequestInFlight = false
             return .cancel(id: NumberFactRequestID.self)
+            
+        case .stepperChanged(let value):
+            state.count = value
+            state.currentFact = nil
+            state.isFactRequestInFlight = false
+            return .cancel(id: NumberFactRequestID.self)
+            
+        case .factButtonTapped:
+            state.currentFact = nil
+            state.isFactRequestInFlight = true
+            
+            return .task { [count = state.count] in
+                await .factResponse(TaskResult { try await
+                    self.factClient.fetch(count) })
+            }
+            .cancellable(id: NumberFactRequestID.self)
+            
+        case .factResponse(.success(let response)):
+            state.isFactRequestInFlight = false
+            state.currentFact = response
+            return .none
+            
+        case .factResponse(.failure):
+            state.isFactRequestInFlight = false
+            return .none
         }
     }
 }
 
 struct EffectsCancellationView: View {
+    let store: StoreOf<EffectsCancellation>
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            Form {
+                Section {
+                    Stepper("\(viewStore.count)",
+                            value: viewStore.binding(get: \.count, send: EffectsCancellation.Action.stepperChanged)
+                    )
+                    
+                    if viewStore.isFactRequestInFlight {
+                        HStack {
+                            Button("Cancel") {
+                                viewStore.send(.cancelButtonTapped)
+                            }
+                            Spacer()
+                            ProgressView()
+                                .id(UUID())
+                        }
+                    } else {
+                        Button("Number fact") {
+                            viewStore.send(.factButtonTapped)
+                        }
+                        .disabled(viewStore.isFactRequestInFlight)
+                    }
+                    
+                    viewStore.currentFact.map {
+                        Text($0).padding(.vertical, 8)
+                    }
+                }
+                Section {
+                    Button("Number facts provided by numbersapi.com") {
+                        self.openURL(URL(string: "http://numbersapi.com")!)
+                    }
+                    .foregroundStyle(.secondary)
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .buttonStyle(.borderless)
+        }
+        .navigationTitle("Effect Cancellation")
     }
 }
 
 struct EffectsCancellation_Previews: PreviewProvider {
     static var previews: some View {
-        EffectsCancellationView()
+        NavigationView{
+            EffectsCancellationView(store: Store(initialState: EffectsCancellation.State(), reducer: EffectsCancellation()))
+        }
     }
 }
