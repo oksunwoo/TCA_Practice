844447b1ef96309f5071aa6dffecb4eae5a374bb
oksunwoo <oksunwo@naver.com>
2022-11-17 22:41:55 +0900

    feat: numberFactView 추가

---
+++ TCA_Practice/TCA_Practice/NumberFactView.swift
@@ -0,0 +1,20 @@
+//
+//  NumberFactView.swift
+//  TCA_Practice
+//
+//  Created by Sunwoo on 2022/11/17.
+//
+
+import SwiftUI
+
+struct NumberFactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NumberFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        NumberFactView()
+    }
+}
