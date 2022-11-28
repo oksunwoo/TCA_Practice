05cf3b29e33f49bd216ab506cdc0cea06fcd8f48
oksunwoo <oksunwo@naver.com>
2022-11-28 17:41:12 +0900

    feat: WeatherInformation 타입 구현

--- TCA_Practice/TCA_Practice/WeatherInformation.swift
+++ TCA_Practice/TCA_Practice/WeatherInformation.swift
@@ -7,6 +7,80 @@
 
 import Foundation
 
-struct WeatherInformation {
-    
+// MARK: - WeatherInformation
+struct WeatherInformation: Codable {
+    let coord: Coord
+    let weather: [Climate]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let rain: Rain
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity, seaLevel, grndLevel: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+    }
+}
+
+// MARK: - Rain
+struct Rain: Codable {
+    let the1H: Double
+
+    enum CodingKeys: String, CodingKey {
+        case the1H = "1h"
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let type, id: Int
+    let country: String
+    let sunrise, sunset: Int
+}
+
+// MARK: - Climate
+struct Climate: Codable {
+    let id: Int
+    let main, weatherDescription, icon: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
 }
