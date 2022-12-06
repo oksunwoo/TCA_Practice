7f7840104daf9d46be9afd96ed2977db3df866c8
oksunwoo <oksunwo@naver.com>
2022-12-06 17:33:33 +0900

    feat: post를 위한 header 작성

--- TCA_Practice/TCA_Practice/PhotoClient.swift
+++ TCA_Practice/TCA_Practice/PhotoClient.swift
@@ -22,6 +22,25 @@ extension DependencyValues {
 extension PhotoClient: DependencyKey {
     static let liveValue = PhotoClient(
         post: { imageData in
+            var request = URLRequest(url: URL(string: "https://cv-api.kakaobrain.com/pose")!)
+            request.httpMethod = "POST"
+            
+            let boundary = UUID().uuidString
+            let headers: [String: String] = ["Content-Type": "multipart/form-data;boundary=\(boundary)", "Authorization": "KakaoAK 32e827cd3335958e97b7b7240ec4466c"]
+            
+            headers.forEach { (key, value) in
+                request.setValue(value, forHTTPHeaderField: key)
+            }
+            
+            var body = Data()
+            let boundaryPrefix = "--\(boundary)\r\n"
+            
+            body.append(boundaryPrefix.data(using: .utf8)!)
+            body.append("Content-Disposition: form-data; name=\"file\"r\n".data(using: .utf8)!)
+            //body.append("Content-Type: application/json\r\n\r\n".data(using: .utf8)!)
+            //body.append(json)
+            body.append("\r\n".data(using: .utf8)!)
+            
             let (data, _) = try await URLSession.shared.data(from: URL(string: "https://cv-api.kakaobrain.com/pose")!)
             
             return String(decoding: data, as: UTF8.self)
